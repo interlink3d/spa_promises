2ca4893773312b3c2ecee00ec8b7b36d9d6991dd
Edgar Barajas <ebarajas05@gmail.com>
2016-08-18 10:46:13 -0500

    added a js folder to move my js files into

---
+++ js/events.js
@@ -0,0 +1,21 @@
+CarLot = (function(carLot) {
+  carLot.activateEvents = function() {
+
+    var cards = document.querySelectorAll(".carCard")
+
+    cards.forEach(function(card){
+      card.addEventListener("click", function(){
+        var userInput = document.getElementById("userInput")
+        userInput.value = ""
+        userInput.focus()
+        CarLot.resetCards(cards)
+        CarLot.styleCard(card, "color")
+        CarLot.mirrorText(card, userInput)
+      })
+    })
+
+  }
+
+  return carLot
+
+})(CarLot);
